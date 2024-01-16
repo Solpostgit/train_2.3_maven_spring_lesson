de797a3a16b28762da2b9a7ce1adc4157735d5a6
Solpostgit <begemot_off@mail.ru>
2024-01-16 22:16:12 +0300

    Initial commit

---
+++ src/test/java/ru/skypro/train_2/_maven_spring_lesson/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.train_2._maven_spring_lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
